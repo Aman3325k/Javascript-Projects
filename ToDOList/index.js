faf04d86e26a5a7c04fed33efcc83e50e5fc35d6
Aman Pradhan <pradhanaman2004@gmail.com>
2026-04-15 09:23:29 +0530

    Created to do list project that saves info in local storage too

---
+++ ToDOList/index.js
@@ -0,0 +1,64 @@
+function savedata() {
+    const ul = document.querySelector("#list ul");
+
+    // 🔥 FORCE checkbox state into HTML
+    ul.querySelectorAll("input[type='checkbox']").forEach(cb => {
+        if (cb.checked) {
+            cb.setAttribute("checked", "checked");
+        } else {
+            cb.removeAttribute("checked");
+        }
+    });
+
+    localStorage.setItem("tasks", ul.innerHTML);
+}
+
+
+function button()
+{
+const input = document.getElementById("input");
+const ul = document.querySelector("#list ul")
+
+if(input.value.trim() === "") return;
+
+const li = document.createElement('li');
+
+li.innerHTML = `
+            <label>
+                <input type="checkbox">
+                <span class="checkmark"></span>
+                <span class="task-text">${input.value}</span>
+            </label>
+            <span class="delete">X</span>  
+       `;
+       ul.appendChild(li);
+
+       input.value = "";
+       savedata()
+}
+
+// Delete and save data 
+document.addEventListener('click',(e)=>{
+    
+if(e.target.classList.contains("delete")){
+    e.target.parentElement.remove();
+    savedata();
+}
+    
+});
+
+// checked one save in storage 
+document.addEventListener('change',(e)=>{
+    if(e.target.matches('input[type="checkbox"]')){
+        console.log("Checkbox changed:", e.target.checked);
+        savedata();
+    }
+});
+
+// loading data on chrome
+function loaddata(){
+    const ul = document.querySelector("#list ul");
+    ul.innerHTML = localStorage.getItem("tasks")|| "";
+}
+
+loaddata();
